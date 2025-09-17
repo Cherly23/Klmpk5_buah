480982474406a20ec7e3cd095245877b08388f60
ichul <ikizaki01@gmail.com>
2025-09-17 17:10:24 +0700

    ikhul commit

---
+++ src/klmpk5_buah/Manggis.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package klmpk5_buah;
+
+/**
+ *
+ * @author HP
+ */
+public class Manggis {
+        public static void main(String[] args) {
+        Ichul_Manggis Manggis = new Ichul_Manggis();
+        Manggis.nama_buah = "Manggis";
+        Manggis.warna = "Ungu tua";
+        Manggis.rasa = "Manis dan tidak terlalu asam";
+        Manggis.harga = 30000;
+        Manggis.berat = 80;
+        Manggis.kulit = 
+                
+        System.out.println("Nama Buah : " + Manggis.nama_buah);
+    }
+    
+}
