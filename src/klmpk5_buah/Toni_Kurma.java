e531d32dbaf394cc9587027738ffe8f6b382780b
fatoni883 <mimamfatoni2@gmail.com>
2025-09-17 11:03:36 +0700

    Toni_Kurma

---
+++ src/klmpk5_buah/Toni_Kurma.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package klmpk5_buah;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Toni_Kurma {
+    
+}
