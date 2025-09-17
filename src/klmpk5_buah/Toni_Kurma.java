4735dd22740b2df5e2379f235fa952d95d4f3255
fatoni883 <mimamfatoni2@gmail.com>
2025-09-17 16:08:04 +0700

    Toni_Kurma

--- src/klmpk5_buah/Toni_Kurma.java
+++ src/klmpk5_buah/Toni_Kurma.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package klmpk5_buah;
-
-/**
- *
- * @author ASUS
- */
 public class Toni_Kurma {
-    
+    String nama_Buah, warna, rasa;
+    double harga, berat;
+    boolean kulit;
+    public void setNamaBuah(String namaBuah) {
+        this.nama_Buah = namaBuah;
+    }
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    public void setKulit(boolean klt) {
+        this.kulit = klt;
+    }
+    public void setBerat(double berat) {
+        this.berat = berat;
+    }
+    public String getNamaBuah() {
+        return this.nama_Buah;
+    }
+    public String getWarna() {
+        return this.warna;
+    }
+    public String getRasa() {
+        return this.rasa;
+    }
+    public double getHarga() {
+        return this.harga;
+    }
+    public double getBerat() {
+        return this.berat;
+    }
+     public boolean getKulit() {
+        return this.kulit;
+    }
 }
+
