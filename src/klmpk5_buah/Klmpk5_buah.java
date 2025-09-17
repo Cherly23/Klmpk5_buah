ccc3fd1e2f5a8509533b469de71e6e2ab5630236
giantluigi <azkabaek5@gmail.com>
2025-09-17 16:31:51 +0700

    pushh cherr !!! BUAHH APELL

--- src/klmpk5_buah/Klmpk5_buah.java
+++ src/klmpk5_buah/Klmpk5_buah.java
@@ -9,12 +9,38 @@ package klmpk5_buah;
  * @author cherly
  */
 public class Klmpk5_buah {
+    private String nama;
+    private String warna;
+    private double berat;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    
+    public Klmpk5_buah(String nama, String warna, double berat) {
+        this.nama = nama;
+        this.warna = warna;
+        this.berat = berat;
+    }  
+    public String getNama(){
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getwarna() {
+        return warna;
+    }
+    public void setwarna(String warna){
+        this.warna = warna;
+    }
+    public double getBerat() {
+        return berat;
+    }
+    public void setberat(double berat) {
+        this.berat = berat;
+    }
+    public void infobuah() {
+        System.out.println("Nama: "+ nama+",warna:"+ warna + ",Berat: "+ berat + "kg");
+    }
+        
     }
     
-}
+
