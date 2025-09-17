582cee5ac0c4e4e4f84902b148a9ac7940657e26
Cherly <cherlyu664@gmail.com>
2025-09-17 09:26:53 +0700

    first commit

---
+++ src/klmpk5_buah/Klmpk5_buah.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package klmpk5_buah;
+
+/**
+ *
+ * @author cherly
+ */
+public class Klmpk5_buah {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
