a2ba40ac2d3ec46e2372fa9d171419e60c014ba7
Cherly <cherlyu664@gmail.com>
2025-09-18 05:37:48 +0700

    branch commit5

---
+++ src/klmpk5_buah/AZKA_APEL.java
@@ -0,0 +1,46 @@
+package klmpk5_buah;
+
+public class AZKA_APEL {
+    private String nama_buah, rasa, warna;
+    boolean kulit;
+    double harga, berat;
+    
+  
+    public void setnama_buah(String nb){
+        this.nama_buah = nb;
+    }
+    public String getnama_buah(){
+        return this.nama_buah;
+    }
+    public void setrasa(String sr){
+        this.rasa = sr; 
+    }
+    public String getrasa(){
+        return this.rasa;
+    }
+    public String getwarna(){
+        return this.warna;    
+    }
+    public void setwarna(String wr){
+        this.rasa = wr;
+    }
+    public void setkulit(boolean kl){
+        this.kulit = kl;
+    }
+    public boolean getkulit(){
+        return this.kulit;
+    }
+    public void setharga(double hrg){
+        this.harga = hrg;
+    }
+    public double getharga(){
+        return this.harga;
+    }
+    public void setberat(double brt){
+        this.berat = brt;
+    }
+    public double getberat() {
+        return this.berat;
+    }
+    
+}
---
+++ src/klmpk5_buah/Cialin_Jeruk.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package klmpk5_buah;
+
+/**
+ *
+ * @author ThinkPad
+ */
+public class Cialin_Jeruk {
+    String nama_buah, warna, rasa;
+    double harga, berat;
+    boolean kulit;
+    public  void  setnama_buah(String nb){
+        this.nama_buah = nb;
+    }
+    public  void  setwarna(String wrn){
+        this.warna = wrn;
+    }
+    public  void  setrasa(String rs){
+        this.rasa = rs;
+    }
+    public  void  setharga(double hrg){
+        this.harga = hrg;
+    }
+    public  void  setberat(double brt){
+        this.berat = brt;
+    }
+    public void setkulit (boolean klt){
+        this.kulit = klt;
+    }
+    public String getnama_buah(){
+        return this.nama_buah;
+    }
+    public String getwarna(){
+    return this.warna;
+}
+    public String getrasa(){
+        return this.rasa;
+    }
+    public double getharga(){
+        return this.harga;
+    }
+    public double getberat(){
+        return this.berat;
+    }
+    public boolean getkulit (){
+        return this.kulit;
+    }
+}
