392c725f65fee5a45d9c63c65ec5a0f09bfb718f
CialinAlin <cialin070@gmail.com>
2025-09-28 11:42:04 +0700

    Cialin_Commit2

--- src/klmpk5_buah/Cialin_Jeruk.java
+++ src/klmpk5_buah/Cialin_Jeruk.java
@@ -12,40 +12,30 @@ public class Cialin_Jeruk {
     String nama_buah, warna, rasa;
     double harga, berat;
     boolean kulit;
-    public  void  setnama_buah(String nb){
-        this.nama_buah = nb;
-    }
-    public  void  setwarna(String wrn){
-        this.warna = wrn;
-    }
-    public  void  setrasa(String rs){
-        this.rasa = rs;
-    }
-    public  void  setharga(double hrg){
-        this.harga = hrg;
-    }
-    public  void  setberat(double brt){
-        this.berat = brt;
-    }
+    public void  setnama_buah(String nb){
+        this.nama_buah = nb;}
+    public void  setwarna(String wrn){
+        this.warna = wrn;}
+    public void  setrasa(String rs){
+        this.rasa = rs;}
+    public void  setharga(double hrg){
+        this.harga = hrg;}
+    public void  setberat(double brt){
+        this.berat = brt;}
     public void setkulit (boolean klt){
-        this.kulit = klt;
-    }
+        this.kulit = klt;}
+
+    
     public String getnama_buah(){
-        return this.nama_buah;
-    }
+        return this.nama_buah;}
     public String getwarna(){
-    return this.warna;
-}
+    return this.warna;}
     public String getrasa(){
-        return this.rasa;
-    }
+        return this.rasa;}
     public double getharga(){
-        return this.harga;
-    }
+        return this.harga;}
     public double getberat(){
-        return this.berat;
-    }
+        return this.berat;}
     public boolean getkulit (){
-        return this.kulit;
-    }
+        return this.kulit;}
 }
--- src/klmpk5_buah/Klmpk5_buah.java
+++ src/klmpk5_buah/Klmpk5_buah.java
@@ -15,9 +15,36 @@ public class Klmpk5_buah {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Cialin_Jeruk buah = new Cialin_Jeruk();
+        Cialin_Jeruk jeruk = new Cialin_Jeruk();
+        jeruk.nama_buah = "Jeruk";
+        jeruk.warna = "Kuning";
+        jeruk.rasa = "Manis dan Asam";
+        jeruk.harga = 20000;
+        jeruk.berat = 1;
+        jeruk.kulit = false ;
         
-       buah.setnama_buah("Jeruk");
-       System.out.println("Nama Buah : " + buah.getnama_buah());
+        System.out.println("=====================================================");
+        System.out.println("                      DATA BUAH");
+        System.out.println("=====================================================");
+        
+        jeruk.setnama_buah("Jeruk");
+        System.out.println("Nama Buah\t\t: " + jeruk.getnama_buah());
+        
+        jeruk.setwarna("Kuning");
+        System.out.println("Warna buah\t\t: " + jeruk.getwarna());
+        
+        jeruk.setrasa("Manis dan Asam");
+        System.out.println("Rasa buah\t\t: " + jeruk.getrasa());
+        
+        jeruk.setharga(20.000);
+        System.out.println("Harga buah per kilo\t: " + jeruk.getharga());
+        
+        jeruk.setberat(1);
+        System.out.println("Berat buah per kilo\t: " + jeruk.getberat());
+        
+        jeruk.setkulit(false);
+        System.out.println("kulit bisa dimakan?\t: " + (jeruk.kulit ? "Ya" : "Tidak"));
+        
+        System.out.println("=====================================================");
     }
     }
