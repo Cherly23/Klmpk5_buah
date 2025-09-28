7483e2800a348ad23348068e99dfb81b3da41ebd
Cherly <cherlyu664@gmail.com>
2025-09-28 13:02:29 +0700

    commit5

--- src/klmpk5_buah/AZKA_APEL.java
+++ src/klmpk5_buah/AZKA_APEL.java
@@ -43,45 +43,5 @@ public class AZKA_APEL {
         return this.berat;
     }
     
-    String nama_buah, rasa, warna;
-    boolean kulit;
-    double harga, berat;
-
-    public String getNama_buah() {
-        return nama_buah;
-    }
-    public void setNama_buah(String nama_buah) {
-        this.nama_buah = nama_buah;
-    }
-    public String getRasa() {
-        return rasa;
-    }
-    public void setRasa(String rasa) {
-        this.rasa = rasa;
-    }
-    public String getWarna() {
-        return warna;
-    }
-    public void setWarna(String warna) {
-        this.warna = warna;
-    }
-    public boolean isKulit() {
-        return kulit;
-    }
-    public void setKulit(boolean kulit) {
-        this.kulit = kulit;
-    }
-    public double getHarga() {
-        return harga;
-    }
-    public void setHarga(double harga) {
-        this.harga = harga;
-    }
-    public double getBerat() {
-        return berat;
-    }
-    public void setBerat(double berat) {
-        this.berat = berat;
-    }
-     
+   
 }
